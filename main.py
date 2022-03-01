22127771d53ab10ef1da2001d39dfe6e2cef326c
toolslog <67196704+toolslog@users.noreply.github.com>
2022-03-01 21:34:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import os
 import json
 
 url = 'https://api.lolicon.app/setu/v2'
-download_num = 10000
+download_num = 37058
+
 headers={
 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
 }
